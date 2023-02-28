3c25bc86fc06f1da7443cbba61d4c652b9ef2c7f
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-02-28 11:44:20 +0545

    secrets module added;

---
+++ modules/secrets/outputs.tf
@@ -0,0 +1,55 @@
+output ssm_django_secret_key_arn {
+    value = data.aws_ssm_parameter.django_secret_key.arn
+}
+
+output ssm_db_name_arn {
+    value = data.aws_ssm_parameter.db_name.arn
+}
+
+output ssm_db_name_value {
+    value = data.aws_ssm_parameter.db_name.value
+}
+
+output ssm_db_username_arn {
+    value = data.aws_ssm_parameter.db_username.arn
+}
+
+output ssm_db_username_value {
+    value = data.aws_ssm_parameter.db_username.value
+}
+
+output ssm_db_password_arn {
+    value = data.aws_ssm_parameter.db_password.arn
+}
+
+output ssm_db_password_value {
+    value = data.aws_ssm_parameter.db_password.value
+}
+
+output ssm_db_port_arn {
+    value = data.aws_ssm_parameter.db_port.arn
+}
+
+output ssm_db_port_value {
+    value = data.aws_ssm_parameter.db_port.value
+}
+
+output ssm_deep_db_name_arn {
+    value = data.aws_ssm_parameter.deep_db_name.arn
+}
+
+output ssm_deep_db_username_arn {
+    value = data.aws_ssm_parameter.deep_db_username.arn
+}
+
+output ssm_deep_db_password_arn {
+    value = data.aws_ssm_parameter.deep_db_password.arn
+}
+
+output ssm_deep_db_port_arn {
+    value = data.aws_ssm_parameter.deep_db_port.arn
+}
+
+output ssm_deep_db_host_arn {
+    value = data.aws_ssm_parameter.deep_db_host.arn
+}
