dcb66cdf835bb507fb49748bc95b17e0a644094a
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-04-11 13:47:45 +0545

    s3 buckets configs added;

---
+++ modules/s3/outputs.tf
@@ -0,0 +1,15 @@
+output "nlp_server_bucket_static_name" {
+    value = aws_s3_bucket.static.id
+}
+
+output "nlp_server_bucket_static_arn" {
+    value = aws_s3_bucket.static.arn
+}
+
+output "task_results_bucket_name" {
+    value = aws_s3_bucket.processed_bucket.id
+}
+
+output "task_results_bucket_arn" {
+    value = aws_s3_bucket.processed_bucket.arn
+}
