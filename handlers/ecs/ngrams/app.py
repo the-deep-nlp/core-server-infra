ca8306acd7cde2832f83086e4c19ec8f0168e066
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-05-03 15:02:23 +0545

    updated the table when callback fails

--- handlers/ecs/ngrams/app.py
+++ handlers/ecs/ngrams/app.py
@@ -6,7 +6,7 @@ import psycopg2
 import requests
 import boto3
 import sentry_sdk
-from datetime import date
+from datetime import date, datetime
 from enum import Enum
 from botocore.client import Config
 from botocore.exceptions import ClientError
@@ -97,6 +97,7 @@ class NGramsGeneratorHandler:
 
         # db table
         self.db_table_name = os.environ.get("DB_TABLE_NAME", None)
+        self.db_table_callback_tracker = os.environ.get("DB_TABLE_CALLBACK_TRACKER", None)
 
         self.entries = self._download_prepare_entries()
 
@@ -189,6 +190,9 @@ class NGramsGeneratorHandler:
             return None
 
     def ngrams_summary_store_local(self, ngrams_summary):
+        """
+        Store the result in tmp directory
+        """
         filepath = f"/tmp/ngrams/{self.client_id}"
         with open(filepath, "w") as f:
             f.write(ngrams_summary)
@@ -214,6 +218,19 @@ class NGramsGeneratorHandler:
                 if db_conn is not None:
                     db_conn.close()
 
+    def _update_db_table_callback_retry(self):
+        """
+        Updates the table whenever the callback fails
+        """
+        if self.db_table_callback_tracker and self.ngrams_id:
+            now_date = datetime.now().isoformat()
+            self.status_update_db(
+                sql_statement=f""" INSERT INTO {self.db_table_callback_tracker} 
+                                (request_unique_id, created_at, modified_at, retries_count, status) 
+                                VALUES ('{self.ngrams_id}', '{now_date}', '{now_date}', 0, 3) """  # status = 3(Retrying)
+            )
+            logging.info("Updated the db table for callback retries.")
+
     def send_request_on_callback(self, presigned_url, status):
         """
         Sends the results in a callback url
@@ -231,10 +248,12 @@ class NGramsGeneratorHandler:
             )
         except requests.exceptions.RequestException as rexc:
             logging.error("Exception occurred while sending request %s", str(rexc))
+            self._update_db_table_callback_retry()
         if response.status_code == 200:
             logging.info("Successfully sent the request on callback url")
         else:
             logging.error("Error while sending the request on callback url")
+            self._update_db_table_callback_retry()
 
     def dispatch_results(self, status, presigned_url=None):
         """
@@ -246,11 +265,13 @@ class NGramsGeneratorHandler:
             self.status_update_db(
                 sql_statement=f""" UPDATE {self.db_table_name} SET status='{status}', result_s3_link='{presigned_url}' WHERE unique_id='{self.ngrams_id}' """
             )
+            logging.info("Updated the db table with event status %s", str(status))
         elif self.db_table_name:
             # Presigned url generation failed
             self.status_update_db(
                 sql_statement=f""" UPDATE {self.db_table_name} SET status='{status}' WHERE unique_id='{self.ngrams_id}' """
             )
+            logging.info("Updated the db table with event status %s", str(status))
         else:
             logging.error("Callback url / presigned s3 url / Database table name are not found.")
 
