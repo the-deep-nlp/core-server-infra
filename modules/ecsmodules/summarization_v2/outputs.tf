d0ddc0cc3e64605808ea9afcbaabbf94599a1bbd
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-06-01 20:42:29 +0545

    endpoint fixes;

--- modules/ecsmodules/summarization_v2/outputs.tf
+++ modules/ecsmodules/summarization_v2/outputs.tf
@@ -7,5 +7,5 @@ output "summarization_v2_container_name" {
 }
 
 output "aws_service_discovery_service_endpoint" {
-  value = "http://${var.local_sub_domain}.${var.private_dns_namespace_local_domain}:${var.app_port}"
+  value = "http://${var.local_sub_domain}-${var.environment}.${var.private_dns_namespace_local_domain}:${var.app_port}"
 }
