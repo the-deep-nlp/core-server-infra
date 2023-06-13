bb4f572f4ad35e4d82f1472b927d8baf4a1fecec
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2023-06-13 11:53:30 +0545

    removed methods as they are served from utils module;

--- handlers/ecs/summarization_v2/app.py
+++ handlers/ecs/summarization_v2/app.py
@@ -1,22 +1,28 @@
 import os
-import io
 import json
 import logging
-import psycopg2
 import requests
 import boto3
 import sentry_sdk
 from pathlib import Path
-from datetime import date, datetime
-from enum import Enum
+from datetime import date
 from botocore.client import Config
-from botocore.exceptions import ClientError
 from huggingface_hub import snapshot_download
 from reports_generator import ReportsGenerator
 
 from fastapi import FastAPI, BackgroundTasks
 from pydantic import BaseModel
 from typing import Union
+from nlp_modules_utils import (
+    Database,
+    StateHandler,
+    prepare_sql_statement_success,
+    prepare_sql_statement_failure,
+    status_update_db,
+    upload_to_s3,
+    send_request_on_callback,
+    update_db_table_callback_retry
+)
 
 logging.getLogger().setLevel(logging.INFO)
 
@@ -72,50 +78,6 @@ async def gen_report(item: InputStructure, background_tasks: BackgroundTasks):
     return {
         "message": "Task received and running in background."
     }
-
-
-class ReportStatus(Enum):
-    """
-    List of categories to indicate the status of task
-    """
-    INITIATED = 1
-    SUCCESS = 2
-    FAILED = 3
-    INPUT_URL_PROCESS_FAILED = 4
-
-class Database:
-    """
-    Class to handle database connections
-    """
-    def __init__(
-        self,
-        endpoint: str,
-        database: str,
-        username: str,
-        password: str,
-        port: int=5432,
-    ):
-        self.endpoint = endpoint
-        self.database = database
-        self.username = username
-        self.password = password
-        self.port = port
-
-    def __call__(self):
-        try:
-            conn = psycopg2.connect(
-                host=self.endpoint,
-                port=self.port,
-                database=self.database,
-                user=self.username,
-                password=self.password
-            )
-            cur = conn.cursor()
-            return conn, cur
-        except Exception as exc:
-            logging.error("Database connection failed %s", exc)
-            return None, None
-
 
 class ReportsGeneratorHandler:
     """
@@ -208,121 +170,55 @@ class ReportsGeneratorHandler:
                 return {}
         return model_info
 
-    def generate_presigned_url(self, bucket_name, key):
-        """
-        Generates a presigned url of the file stored in s3
-        """
-        # Note that the bucket and service(e.g. summarization) should run on the same aws region
-        try:
-            url = s3_client.generate_presigned_url(
-                ClientMethod="get_object",
-                Params={
-                    "Bucket": bucket_name,
-                    "Key": key
-                },
-                ExpiresIn=self.signed_url_expiry_secs
-            )
-        except ClientError as cexc:
-            logging.error("Error while generating presigned url %s", cexc)
-            return None
-        return url
-
-    def summary_store_s3(self, summary, bucket_name="test-ecs-parser11", key="summary.txt"):
-        """
-        Stores the summary in s3
-        """
-        try:
-            session = boto3.Session()
-            s3_resource = session.resource("s3")
-            bucket = s3_resource.Bucket(bucket_name)
-            summary_bytes = bytes(summary, "utf-8")
-            summary_bytes_obj = io.BytesIO(summary_bytes)
-            bucket.upload_fileobj(
-                summary_bytes_obj,
-                key,
-                ExtraArgs={"ContentType": "text/plain; charset=utf-8"}
-            )
-            return self.generate_presigned_url(bucket_name, key)
-        except ClientError as cexp:
-            logging.error(str(cexp))
-            return None
-
-
-    def status_update_db(self, sql_statement):
-        """
-        Updates the status in the database
-        """
-        db = Database(**self.db_config)
-        db_conn, db_cursor = db()
-        if db_cursor:
-            try:
-                db_cursor.execute(sql_statement)
-                logging.info("Db updated. Number of rows affected: %s", db_cursor.rowcount)
-                db_conn.commit()
-                db_cursor.close()
-            except (Exception, psycopg2.DatabaseError) as error:
-                logging.error(error)
-            finally:
-                if db_conn is not None:
-                    db_conn.close()
-    
-    def _update_db_table_callback_retry(self, summarization_id):
-        """
-        Updates the table whenever the callback fails
-        """
-        if self.db_table_callback_tracker and summarization_id:
-            now_date = datetime.now().isoformat()
-            self.status_update_db(
-                sql_statement=f""" INSERT INTO {self.db_table_callback_tracker} 
-                                (request_unique_id, created_at, modified_at, retries_count, status) 
-                                VALUES ('{summarization_id}', '{now_date}', '{now_date}', 0, 3) """  # status = 3(Retrying)
-            )
-            logging.info("Updated the db table for callback retries.")
-
-    def send_request_on_callback(self, client_id, callback_url, summarization_id, presigned_url, status):
-        """
-        Sends the results in a callback url
-        """
-        try:
-            response = requests.post(
-                callback_url,
-                headers=self.headers,
-                data=json.dumps({
-                    "client_id": client_id,
-                    "presigned_s3_url": presigned_url,
-                    "status": status
-                }),
-                timeout=30
-            )
-        except requests.exceptions.RequestException as rexc:
-            logging.error("Exception occurred while sending request %s", str(rexc))
-            self._update_db_table_callback_retry(summarization_id)
-        if response.status_code == 200:
-            logging.info("Successfully sent the request on callback url")
-        else:
-            logging.error("Error while sending the request on callback url")
-            self._update_db_table_callback_retry(summarization_id)
-
     def dispatch_results(self, client_id, summarization_id, callback_url, status, presigned_url=None):
         """
         Dispatch results to callback url or write to database
         """
+        response_data = {
+            "client_id": client_id,
+            "presigned_s3_url": presigned_url,
+            "status": status
+        }
         if callback_url:
-            self.send_request_on_callback(client_id, callback_url, summarization_id, presigned_url=presigned_url, status=status)
-        if presigned_url and self.db_table_name: # update for presigned url
-            self.status_update_db(
-                sql_statement=f""" UPDATE {self.db_table_name} SET status='{status}', result_s3_link='{presigned_url}' WHERE unique_id='{summarization_id}' """
+            callback_response = send_request_on_callback(
+                callback_url,
+                response_data=response_data,
+                headers=self.headers
             )
+            if not callback_response:
+                db_client = Database(**self.db_config)
+                db_conn, db_cursor = db_client.db_connection()
+                update_db_table_callback_retry(
+                    db_conn,
+                    db_cursor,
+                    summarization_id,
+                    self.db_table_callback_tracker
+                )
+        # Setup database connections
+        db_client = Database(**self.db_config)
+        db_conn, db_cursor = db_client.db_connection()
+
+        if presigned_url and self.db_table_name: # update for presigned url
+            sql_statement = prepare_sql_statement_success(
+                summarization_id,
+                self.db_table_name,
+                status,
+                response_data
+            )
+            status_update_db(db_conn, db_cursor, sql_statement)
             logging.info("Updated the db table with event status %s", str(status))
         elif self.db_table_name:
             # Presigned url generation failed
-            self.status_update_db(
-                sql_statement=f""" UPDATE {self.db_table_name} SET status='{status}' WHERE unique_id='{summarization_id}' """
+            sql_statement = prepare_sql_statement_failure(
+                summarization_id,
+                self.db_table_name,
+                status
             )
+            status_update_db(db_conn, db_cursor, sql_statement)
             logging.info("Updated the db table with event status %s", str(status))
         else:
             logging.error("Callback url / presigned s3 url / Database table name are not found.")
-    
+
     def load_model(self, model_info):
         """
         Loads the summarization model
@@ -338,24 +234,28 @@ class ReportsGeneratorHandler:
 
     def __call__(self, client_id, entries, summarization_id, callback_url):
         if not entries:
-            self.dispatch_results(client_id, summarization_id, callback_url, status=ReportStatus.INPUT_URL_PROCESS_FAILED.value)
+            self.dispatch_results(client_id, summarization_id, callback_url, status=StateHandler.INPUT_URL_PROCESS_FAILED.value)
             return
 
         if self.repgenerator:
             summary = self.repgenerator(entries)
             date_today = date.today().isoformat()
-            presigned_url = self.summary_store_s3(
-                summary=summary,
+            presigned_url = upload_to_s3(
+                contents=summary,
+                contents_type="text/plain; charset=utf-8",
                 bucket_name=self.bucket_name,
-                key=f"summarization/{date_today}/{summarization_id}/summary.txt"
+                key=f"summarization/{date_today}/{summarization_id}/summary.txt",
+                aws_region=os.environ.get("AWS_REGION", "us-east-1"),
+                s3_client=s3_client,
+                signed_url_expiry_secs=self.signed_url_expiry_secs
             )
 
             if presigned_url:
-                self.dispatch_results(client_id, summarization_id, callback_url, status=ReportStatus.SUCCESS.value, presigned_url=presigned_url)
+                self.dispatch_results(client_id, summarization_id, callback_url, status=StateHandler.SUCCESS.value, presigned_url=presigned_url)
             else:
-                self.dispatch_results(client_id, summarization_id, callback_url, status=ReportStatus.FAILED.value)
+                self.dispatch_results(client_id, summarization_id, callback_url, status=StateHandler.FAILED.value)
         else:
-            self.dispatch_results(client_id, summarization_id, callback_url, status=ReportStatus.FAILED.value)
+            self.dispatch_results(client_id, summarization_id, callback_url, status=StateHandler.FAILED.value)
             logging.warning("Summarization models could not be loaded.")
 
 
