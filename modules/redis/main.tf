b934dd1252083423a6f2a3959f29b854fb0a8360
Ranjan Shrestha <ranjan.shrestha.np@gmail.com>
2024-01-25 12:25:14 +0545

    updated the redis version;

--- modules/redis/main.tf
+++ modules/redis/main.tf
@@ -8,10 +8,9 @@ resource "aws_elasticache_cluster" "redis" {
   engine               = "redis"
   node_type            = var.redis_node_type
   num_cache_nodes      = var.redis_num_cache_nodes
-  parameter_group_name = "default.redis3.2"
-  engine_version       = "3.2.10"
+  parameter_group_name = "default.redis7"
+  engine_version       = "7.0"
   port                 = var.redis_port
   subnet_group_name    = aws_elasticache_subnet_group.active.name
   security_group_ids   = [aws_security_group.allow_redis.id]
 }
-
